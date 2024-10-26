0c35ff6b1155510fb4993f2c1c2eb71ecc4331ac
nagi0705 <bellreuben1126@gmail.com>
2024-10-26 15:02:49 +0900

    Update proxy settings for Vercel

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -1,16 +1,15 @@
-// pages/api/proxy.js
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export default createProxyMiddleware({
-  target: process.env.NEXT_PUBLIC_APPWRITE_URL, // 環境変数からエンドポイントを取得
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api/proxy': '', // `/api/proxy`を削除してAppwriteのルートにリクエストを送信
-  },
+    target: process.env.NEXT_PUBLIC_APPWRITE_URL, // 環境変数からエンドポイントを取得
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/proxy/': '/', // '/api/proxy' をルートにリダイレクト
+    },
 });
 
 export const config = {
-  api: {
-    bodyParser: false,
-  },
+    api: {
+        bodyParser: false,
+    },
 };
