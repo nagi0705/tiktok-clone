b92c4147b39a70d7fa89e841741ff844e141f6b6
nagi0705 <bellreuben1126@gmail.com>
2024-10-26 14:49:26 +0900

    Fix Appwrite configuration and environment variables

--- lib/AppWriteClient.tsx
+++ lib/AppWriteClient.tsx
@@ -3,8 +3,8 @@ import { Client, Account, ID, Databases, Query, Storage } from "appwrite";
 // Appwriteクライアントの初期化
 const client = new Client();
 client
-    .setEndpoint(String(process.env.NEXT_PUBLIC_APPWRITE_URL)) // .envのエンドポイントを使用
-    .setProject(String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)); // .envのプロジェクトIDを使用
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_URL || '') // .envのエンドポイントを使用
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || ''); // .envのプロジェクトIDを使用
 
 const account = new Account(client);
 const database = new Databases(client);   
